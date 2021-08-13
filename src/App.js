d23fc79bd1e3b068925d276171fb1c27e58edc4d
bakkaSansanBakka <polina.potapeyko@mail.ru>
2021-08-14 00:24:52 +0300

    minor changes made in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 
 import AddUser from "./components/Users/AddUser";
 
-import "./App.css";
+//import "./App.css";
 
 const App = () => {
   return (
