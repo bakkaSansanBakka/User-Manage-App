08d81df22a747d0d7cd62f645aa7fba6a80022e0
bakkaSansanBakka <polina.potapeyko@mail.ru>
2021-08-14 00:29:38 +0300

    add Button component to AddUser component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,23 @@
 import React from "react";
+
+import styles from "./AddUser.module.css";
+
 import Card from "../UI/Card";
+import Button from "../UI/Button";
+
 const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
 
   return (
-    <Card>
+    <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
         <input id="username" type="text" />
         <label htmlFor="age">Age (Years)</label>
         <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
