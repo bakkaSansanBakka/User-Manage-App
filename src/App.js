794333f4e3e9f4ce1bac4c736c9b99eee966e43b
bakkaSansanBakka <polina.potapeyko@mail.ru>
2021-08-18 20:51:22 +0300

    using portals to get more semantically correct html code: port overlay and backdrop components next to main users div

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 
 import AddUser from "./components/Users/AddUser";
 import UserList from "./components/Users/UserList";
@@ -18,10 +18,10 @@ const App = () => {
   };
 
   return (
-    <>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UserList users={usersList} />
-    </>
+    </Fragment>
   );
 };
 
